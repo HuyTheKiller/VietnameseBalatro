f209a0d9f3dc0d5a24f9501b53d62c2c8b38e7b6
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-09-11 22:01:50 +0700

    Move to overridden font

--- steamodded.lua
+++ steamodded.lua
@@ -8,15 +8,7 @@ SMODS.Atlas({
 SMODS.Language({
     key = "vi",
     label = "Tiếng Việt",
-    font = {
-        file = "m6x11plus.ttf",
-        render_scale = G.TILESIZE*10,
-        TEXT_HEIGHT_SCALE = 0.83,
-        TEXT_OFFSET = {x=10,y=-20},
-        FONTSCALE = 0.1,
-        squish = 1,
-        DESCSCALE = 1,
-    },
+    font = 1,
     beta = true,
     button = "Phản Hồi Ngôn Ngữ (vô dụng)",
     warning = {"Đây là bản dịch cá nhân và được chèn vào game thông qua Steamodded (smods).", "Nếu có thắc mắc, vui lòng liên hệ chủ bản dịch", "dưới tên theambushingbush trong máy chủ Balatro chính thức."},
